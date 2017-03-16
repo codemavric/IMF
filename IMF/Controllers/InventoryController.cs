37084be20fb2011f4893f01c71d3abe0d9cc17e0
codemavric <vicram.purshotam@gmail.com>
2017-03-16 11:41:40 +0200

    Add files to repository

---
+++ IMF/Controllers/InventoryController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IMF.Models;
+
+namespace IMF.Controllers
+{
+    public class InventoryController : Controller
+    {
+        // GET: Inventory
+
+        [HttpGet]
+        public ActionResult Input()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Input(IMFInventory item)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.ItemDetails =
+
+                    $"Cost Price: {item.costPrice.ToString("C")}" + "<br/> <br/>" +
+                    $"Markup: {item.markup}" + "%" + "<br/> <br/>" +
+                    $"Selling Price: {item.calcSellingPrice().ToString("C")}";
+
+                return View("Output");
+
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ IMF/Models/IMFInventory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace IMF.Models
+{
+    public class IMFInventory
+    {
+
+        [Required(ErrorMessage = "Please enter in a valid cost price"), ]
+        public double costPrice {get; set;}
+
+        [Required(ErrorMessage ="Please enter in a valid markup")]
+        public double markup { get; set; }
+
+        public double calcSellingPrice()
+        {
+            return ((markup / 100) * costPrice) + costPrice;
+        }
+     
+    }
+}
